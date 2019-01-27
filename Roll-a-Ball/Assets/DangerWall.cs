27a1d48925ded463e496ec74e955106956688083
otamajakusi <hiroyuki.obinata@gmail.com>
2019-01-27 14:18:45 +0900

    initial commit

---
+++ Roll-a-Ball/Assets/DangerWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+    // オブジェクトと接触した時に呼ばれるコールバック
+    void OnCollisionEnter (Collision hit)
+    {
+        int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+        if (count == 0) {
+            return;
+        }
+
+        // 何かの処理
+        if (hit.gameObject.CompareTag("Player")) {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+    }
+}
---
+++ Roll-a-Ball/Assets/Script/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;  // ターゲットへの参照
+    public Vector3 offset;  // 相対座標
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //自分自身とtargetとの相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 自分の座標にtargetの座標を代入する
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
